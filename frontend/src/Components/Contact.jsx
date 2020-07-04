4f2548e96929a3b4f0c1714a29c9870e15a23f91
tbriany <brianytaveras@pursuit.org>
2020-07-03 23:34:06 -0400

    updates styling on contacts page

--- frontend/src/Components/Contact.jsx
+++ frontend/src/Components/Contact.jsx
@@ -7,27 +7,31 @@ import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 
 const Contact = () => {
-    return (
-<section class="hero is-fullheight" id="contact" style={{backgroundColor: "#A6D1C9"}}>
-  <div class="hero-body">
-    <div class="container">
+  return (
+    <section class="hero is-fullheight" id="contact" style={{ backgroundColor: "#A6D1C9" }}>
+      <div class="hero-body">
+        <div class="container">
 
-    <div class="columns is-mobile">
-   <div class="column is-half is-offset-one-quarter">
-   <p class="title" style={{color: "#E83C38"}}>Send me a message!</p>
-      <MyForm/>
-      <a href="https://github.com/tbriany" target="_blank" rel="noopener noreferrer" style={{color: "black"}}>
-      <FontAwesomeIcon icon={faGithub} className="icon is-large"/>
-      </a>
-      <a href="https://www.linkedin.com/in/briany-taveras-457696191/" target="_blank" rel="noopener noreferrer" style={{color: "black"}}>
-      <FontAwesomeIcon icon={faLinkedin} className="icon is-large"/>
-      </a>
-   </div>
-   </div>
-  </div>
-  </div>
-</section>
- );
+          <div class="columns is-mobile">
+            <div class="column is-half is-offset-one-quarter">
+              <h1 class="title">Send me a message!</h1>
+              <MyForm />
+
+              <div className="contactIcons">
+              <a href="https://github.com/tbriany" target="_blank" rel="noopener noreferrer" style={{ color: "black" }}>
+                <FontAwesomeIcon icon={faGithub} className="icon is-large" />
+              </a>
+              <a href="https://www.linkedin.com/in/brianytaveras/" target="_blank" rel="noopener noreferrer" style={{ color: "black" }}>
+                <FontAwesomeIcon icon={faLinkedin} className="icon is-large" />
+              </a>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Contact;
