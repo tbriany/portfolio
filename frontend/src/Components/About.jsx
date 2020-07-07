f6e7a73f8b1c9c0b2a551347ca7b96f82f9c41dc
tbriany <brianytaveras@pursuit.org>
2020-07-07 19:14:39 -0400

    makes navbar transparent

--- frontend/src/Components/About.jsx
+++ frontend/src/Components/About.jsx
@@ -10,7 +10,8 @@ const About = () => {
                 <div class="container">
 
                     <div class="columns">
-                        <div class="column is-full has-text-centered is-size-1-mobile" >                                
+                        {/* <div class="column is-full has-text-centered" > */}
+                        <div class="column is-full has-text-centered" >                                
                                 <h1>Briany Taveras</h1>
                         </div>
                     </div>
--- frontend/src/Components/NavBar.jsx
+++ frontend/src/Components/NavBar.jsx
@@ -1,33 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const NavBar = () => {
-    return (
-      <div>
-            <nav className="navbar is-fixed-top" style={{backgroundColor: "transparent"}}>
-              <div className="container">
-                <div id="navbarMenuHeroA" className="navbar-menu is-active">
-                  <div className="navbar-end">
-                    <a className="navbar-item" href="#about" style={{color: "#20214D", fontWeight: "bold"}}>
-                      ABOUT
+  const [navbarClass, setnavbarClass] = useState("navbar-menu")
+  const [navColor, setnavColor] = useState("navbar is-fixed-top")
+
+  const updateNavbar = () => {
+    if (navbarClass === "navbar-menu") {
+    setnavbarClass("navbar-menu is-active")
+    setnavColor("navbar is-fixed-top has-background-white")
+    } else {
+    setnavbarClass("navbar-menu")
+    setnavColor("navbar is-fixed-top")
+    }
+  }
+  
+  
+  return (
+    <div>
+      <nav className={navColor} role="navigation" aria-label="main navigation" style={{ backgroundColor: "transparent"}}>
+        <div class="navbar-brand">
+          <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" onClick={updateNavbar}>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+
+        </div>
+        <div class={navbarClass}>
+          <div className="navbar-end">
+            <a className="navbar-item" href="#about" style={{ color: "#20214D", fontWeight: "bold"}}>
+              ABOUT
                     </a>
-                    <a className="navbar-item" href={"https://docs.google.com/document/d/1dbJKrAa9IYTICVdQSB8mjYfToaf4VuzbRC64AcJR62o/edit?usp=sharing"} target="_blank" rel="noopener noreferrer" style={{color: "#20214D", fontWeight: "bold"}}>
-                      RESUME
+            <a className="navbar-item" href={"https://docs.google.com/document/d/1dbJKrAa9IYTICVdQSB8mjYfToaf4VuzbRC64AcJR62o/edit?usp=sharing"} target="_blank" rel="noopener noreferrer" style={{ color: "#20214D", fontWeight: "bold" }}>
+              RESUME
                     </a>
-                    <a className="navbar-item" href="#skills" style={{color: "#20214D", fontWeight: "bold"}}>
-                      SKILLS
+            <a className="navbar-item" href="#skills" style={{ color: "#20214D", fontWeight: "bold" }}>
+              SKILLS
                     </a>
-                    <a className="navbar-item" href="#projects" style={{color: "#20214D", fontWeight: "bold"}}>
-                      PROJECTS
+            <a className="navbar-item" href="#projects" style={{ color: "#20214D", fontWeight: "bold" }}>
+              PROJECTS
                     </a>
-                    <a className="navbar-item" href="#contact" style={{color: "#20214D", fontWeight: "bold"}}>
-                      CONTACT
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </nav>
-            </div>
-          );   
+            <a className="navbar-item" href="#contact" style={{ color: "#20214D", fontWeight: "bold" }}>
+              CONTACT
+              </a>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
 }
 
 export default NavBar;
